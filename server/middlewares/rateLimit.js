3ca4b14223112bf50c3fe19d751f8c1fd41b4de2
Sumit Sharma <sumit901693@gmail.com>
2025-08-14 13:32:04 +0530

    rate limit & client origin

--- server/middlewares/rateLimit.js
+++ server/middlewares/rateLimit.js
@@ -4,5 +4,8 @@ export const apiLimiter = rateLimit({
   windowMs: 60 * 1000,
   max: 60,
   standardHeaders: true,
-  legacyHeaders: false
+  legacyHeaders: false,
+  keyGenerator: (req) => {
+    return req.ip; // Uses IP after trust proxy resolution
+  }
 });
